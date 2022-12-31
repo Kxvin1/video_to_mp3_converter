1baf1e276efd9b1f3df9898147f3d9a90f0cad0b
kevin <kevinzy17@gmail.com>
2022-12-30 17:03:09 -0800

    gateway service code started

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -22,3 +22,58 @@ fs = gridfs.GridFS(mongo.db)
 # make our communication with RabbitMQ synchronous
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
+
+
+# login route
+@server.route("/login", methods=["POST"])
+def login():
+    # the request is from flask and we're importing it above
+    # 'access' is a package we created and imported (located in ./auth_svc/access.py) -- the return is a tuple
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+
+
+# upload route/end point
+@server.route("/upload", methods=["POST"])
+def upload():
+    # 'validate' is a package we created and imported (located in ./auth/validate.py) -- the return is a tuple
+    # access will resolve to 'response.text' that contains that JSON string that contains our payload with our claims
+    access, err = validate.token(request)
+
+    # json.loads convert this access json string to a python object (so we can work with it)
+    access = json.loads(access)
+
+    if access["admin"]:
+        # we only want 1 file from the request.files dictionary
+        if len(request.files) > 1 or len(request.files) < 1:
+            return "exactly 1 file required", 400
+
+        for key, f in request.files.items():
+            # 'util' is a package we created and imported (located in ./storage/util.py) -- the return is None or an error
+            # upload takes 4 params: our file 'f', gridfs 'fs' for increased size (scale), rabbitmq channel 'channel', and access 'access'
+            # it will return an err if there is something wrong, otherwise it will return None
+            err = util.upload(f, fs, channel, access)
+
+            if err:
+                return err
+
+        return "success", 200
+    else:
+        return "not authorized", 401
+
+
+# download end point/route
+@server.route("/download", methods=["GET"])
+def download():
+    pass
+
+
+# The if __name__ == "__main__": block is often used to specify code that should only be run when the module is
+# executed as a script, rather than imported as a module. This allows you to use the same module in multiple
+# places without running the code in the if block every time the module is imported.
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=8080)
---
+++ src/gateway/storage/util.py
@@ -0,0 +1,45 @@
+import pika
+import json
+
+
+def upload(f, fs, channel, access):
+    try:
+        # if this put is successful, a file id object (fid) is going to be returned
+        fid = fs.put(f)
+    except Exception as err:
+        # if not successful, print and catch the error
+        print(err)
+        return "internal server error", 500
+
+    # if the file put/update was successful, we need to put a message onto our queue
+    # create the message with these key-value pairs
+    message = {
+        # convert fid obj into a str
+        "video_fid": str(fid),
+        # for now it's None, but it will be changed later in the downstream
+        "mp3_fid": None,
+        # username to identify who owns the file, access comes from our auth svc
+        "username": access["username"],
+    }
+
+    # try and put the message on the queue
+    try:
+        # use channel (rabbitmq) that's passed into the upload function
+        # our rabbitmq by default dispatches messages to our consuming services using the round robin algorithm,
+        # which means that the messages will be distributed evenly amongst our consumers
+        channel.basic_publish(
+            # empty string means default exchange (rabbitmq), which means we set our routing key to the name of the queue
+            # that we want our message to be directed to, and set the exchange to the default exchange
+            # and that will result to our exchange going to the route specified by our routing key
+            exchange="",
+            routing_key="video",
+            # json.dumps converts the obj to a json formatted str
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        print(err)
+        fs.delete(fid)
+        return "internal server error", 500
